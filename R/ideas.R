68c0855df8577c4000e3ad691108f2d2d5afb1bf
catalamarti <marticatalasabate@gmail.com>
2024-01-14 20:04:38 +0000

    Initial commit

---
+++ R/ideas.R
@@ -0,0 +1,16 @@
+# person_year by age group and year and sex
+# summarisePersonYears <- function(cdm, ageGroup, yearGroup, sex = FALSE)
+summarisePersonYears  <- function(cdm,
+                                  ageGroup = NULL,
+                                  yearGroup = NULL,
+                                  sex = FALSE,
+                                  cohortName = NULL) {
+  # check input
+
+  # counts
+}
+# domain counts per age_group and year
+# summariseDomain <- function(cdm, ageGroup, yearGroup, sex = FALSE)
+# atc counts per age_group and year
+# summariseAtcCounts
+
