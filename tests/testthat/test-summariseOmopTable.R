11dca1f358f65ede28ae0245fc50b90141007154
Marta Alcalde-Herraiz <marta.alcalde@estudiantat.upc.edu>
2024-06-10 13:22:28 +0100

    summariseOmopTable() release

---
+++ tests/testthat/test-summariseOmopTable.R
@@ -0,0 +1,31 @@
+test_that("summariseOmopTable() works", {
+
+  # Load mock database ----
+  dbName <- "GiBleed"
+  pathEunomia <- here::here("Eunomia")
+  if (!dir.exists(pathEunomia)) {
+    dir.create(pathEunomia)
+  }
+  CDMConnector::downloadEunomiaData(datasetName = dbName, pathToData = pathEunomia)
+  Sys.setenv("EUNOMIA_DATA_FOLDER" = pathEunomia)
+
+  db <- DBI::dbConnect(duckdb::duckdb(), CDMConnector::eunomia_dir())
+
+  cdm <- CDMConnector::cdmFromCon(
+    con = db,
+    cdmSchema = "main",
+    writeSchema = "main",
+    cdmName = dbName
+  )
+
+  # Check all tables work
+  expect_no_error(summariseOmopTable(cdm$observation_period))
+  expect_no_error(summariseOmopTable(cdm$visit_occurrence))
+  expect_no_error(summariseOmopTable(cdm$condition_occurrence))
+  expect_no_error(summariseOmopTable(cdm$drug_exposure))
+  expect_no_error(summariseOmopTable(cdm$procedure_occurrence))
+  expect_no_error(summariseOmopTable(cdm$device_exposure))
+  expect_no_error(summariseOmopTable(cdm$measurement))
+  expect_no_error(summariseOmopTable(cdm$observation))
+  expect_error(summariseOmopTable(cdm$death))
+})
