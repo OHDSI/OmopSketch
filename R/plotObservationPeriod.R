eb9b0dbf75e3f13b31f7c8fbcbfad4fe6e0d79de
Marta Alcalde-Herraiz <marta.alcalde@estudiantat.upc.edu>
2024-06-14 21:25:21 +0100

    export plotObservationPeriod

--- R/plotObservationPeriod.R
+++ R/plotObservationPeriod.R
@@ -23,7 +23,7 @@ plotObservationPeriod <- function(summarisedObservationPeriod){
     dplyr::filter(.data$estimate_name == "count") |>
     dplyr::mutate(individuals_in_observation = as.numeric(.data$estimate_value),
                   time = .data$strata_level) |>
-    ggplot2::ggplot(ggplot2::aes(x = time, y = individuals_in_observation, group = cdm_name, color = cdm_name)) +
+    ggplot2::ggplot(ggplot2::aes(x = .data$time, y = .data$individuals_in_observation, group = .data$cdm_name, color = .data$cdm_name)) +
     ggplot2::geom_point() +
     ggplot2::geom_line() +
     ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1)) +
