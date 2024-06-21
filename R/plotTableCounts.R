f110197f72122a8ff3400b2dc7fb070d1f1ac725
Marta Alcalde-Herraiz <marta.alcalde@estudiantat.upc.edu>
2024-06-21 14:13:53 +0100

    Update plotRecordCount

--- R/plotTableCounts.R
+++ R/plotTableCounts.R
@@ -1,35 +1,38 @@
 #' Create a gt table from a summarised omop_table.
 #'
-#' @param summarisedTableCounts A summarised_result object with the output from summariseTableCounts().
+#' @param summarisedRecordCount A summarised_result object with the output from summariseTableCounts().
 #'
 #' @return A ggplot showing the table counts
 #'
 #' @export
 #'
-plotTableCounts <- function(summarisedTableCounts) {
+plotRecordCount <- function(summarisedRecordCount){
   # Initial checks ----
-  assertClass(summarisedTableCounts, "summarised_result")
+  assertClass(summarisedRecordCount, "summarised_result")
 
-  if(summarisedTableCounts |> dplyr::tally() |> dplyr::pull("n") == 0){
+  if(summarisedRecordCount |> dplyr::tally() |> dplyr::pull("n") == 0){
     cli::cli_warn("summarisedOmopTable is empty.")
     return(
-      summarisedTableCounts |>
+      summarisedRecordCount |>
         ggplot2::ggplot()
     )
   }
 
   # Plot ----
-  summarisedTableCounts |>
+  summarisedRecordCount |>
     dplyr::mutate(count = as.numeric(.data$estimate_value),
                   time = as.Date(.data$variable_level)) |>
-    visOmopResults::splitGroup() |>
-    ggplot2::ggplot(ggplot2::aes(x = .data$time, y = .data$count, group = .data$omop_table, color = .data$omop_table)) +
+    dplyr::mutate(colour_by = paste0(.data$group_level,"; ",.data$strata_level)) |>
+    ggplot2::ggplot(ggplot2::aes(x = .data$time,
+                                 y = .data$count,
+                                 group = .data$colour_by,
+                                 color = .data$colour_by)) +
     ggplot2::geom_point() +
     ggplot2::geom_line() +
     ggplot2::facet_wrap(facets = "cdm_name") +
     ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1)) +
     ggplot2::xlab("Time") +
     ggplot2::ylab("Counts") +
-    ggplot2::labs(color = "Omop table") +
+    ggplot2::labs(color = "Stratification") +
     ggplot2::theme_bw()
 }
