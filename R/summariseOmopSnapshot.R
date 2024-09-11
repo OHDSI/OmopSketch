3f7d5e56c6803d8af715cf9b1da712f73448104e
Elin Rowlands <elinrow@gmail.com>
2024-09-11 11:02:29 +0100

    Tidy up the function code.

--- R/summariseOmopSnapshot.R
+++ R/summariseOmopSnapshot.R
@@ -1,25 +1,17 @@
 #' Summarise OMOP database and create snapshot
-#'
 #' @param cdm A cdm reference object
 #' @return A summarised result object
 #' @export
 
 summariseOmopSnapshot <- function(cdm) {
-
   summaryTable <- summary(cdm)
 
   summaryTable <- summaryTable |>
-
     omopgenerics::newSummarisedResult(settings = dplyr::tibble(
-
       result_id = unique(summaryTable$result_id),
-
       package_name = "omopSketch",
-
       package_version = as.character(utils::packageVersion("OmopSketch")),
-
       result_type = "summarise_omop_snapshot"
-
     ))
 
   return(summaryTable)
