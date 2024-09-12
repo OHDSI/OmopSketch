544ffb9f1ee86335d1fde70be47396787f8730d0
catalamarti <marticatalasabate@gmail.com>
2024-09-12 16:25:31 +0100

    document

--- R/plotInObservation.R
+++ R/plotInObservation.R
@@ -1,6 +1,6 @@
 #' Create a gt table from a summarised omop_table.
 #'
-#' @param summarisedInObservation Output from summariseInObservation().
+#' @param result A summarised_result object (output of summariseInObservation).
 #' @param facet columns in data to facet. If the facet position wants to be specified, use the formula class for the input
 #' (e.g., strata_level ~ group_level + cdm_name). Variables before "~" will be facet by on horizontal axis, whereas those after "~" on vertical axis.
 #' Only the following columns are allowed to be facet by: "cdm_name", "group_level", "strata_level".
@@ -9,7 +9,9 @@
 #'
 #' @export
 #'
-plotInObservation <- function(summarisedInObservation, facet = NULL){
+plotInObservation <- function(result,
+                              facet = NULL){
+
   internalPlot(summarisedResult = summarisedInObservation,
                facet = facet)
 }
