c37a4f2f40be588b46c342dfabaa9bd501ccd333
Elin Rowlands <elinrow@gmail.com>
2024-09-12 12:27:37 +0100

    Added example to summariseOmopSnapshot

--- R/summariseOmopSnapshot.R
+++ R/summariseOmopSnapshot.R
@@ -2,6 +2,28 @@
 #' @param cdm A cdm reference object
 #' @return A summarised result object
 #' @export
+#' @examples
+#' \donttest{
+#'library(dplyr)
+#'library(CDMConnector)
+#'library(DBI)
+#'library(duckdb)
+#'library(OmopSketch)
+#'
+#'# Connect to Eunomia database
+#'
+#'if (Sys.getenv("EUNOMIA_DATA_FOLDER") == "") Sys.setenv("EUNOMIA_DATA_FOLDER" = tempdir())
+#'if (!dir.exists(Sys.getenv("EUNOMIA_DATA_FOLDER"))) dir.create(Sys.getenv("EUNOMIA_DATA_FOLDER"))
+#'if (!eunomia_is_available()) downloadEunomiaData()
+#'
+#'con <- DBI::dbConnect(duckdb::duckdb(), CDMConnector::eunomia_dir())
+#'
+#'cdm <- CDMConnector::cdmFromCon(con = con, cdmSchema = "main", writeSchema = "main")
+#'
+#'# Run OMOP database snapshot
+#'
+#'snapshot <- summariseOmopSnapshot(cdm)
+#'}
 
 summariseOmopSnapshot <- function(cdm) {
   summaryTable <- summary(cdm)
