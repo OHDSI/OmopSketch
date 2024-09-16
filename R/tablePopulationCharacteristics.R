8a97d6042aba15a153e3cc554682f1507d7f4404
Marta Alcalde-Herraiz <marta.alcalde@estudiantat.upc.edu>
2024-09-16 20:03:19 +0100

    create tablePopulationCharacteristics()

--- R/tablePopulationCharacteristics.R
+++ R/tablePopulationCharacteristics.R
@@ -1,7 +1,11 @@
 tablePopulationCharacteristics <- function(summarisedPopulationCharacteristics){
-
-
-
-
+  summarisedPopulationCharacteristics |>
+    visOmopResults::visOmopTable(hide = c("cohort_name"),
+                                 formatEstimateName = c("N%" = "<count> (<percentage>)",
+                                                        "N" = "<count>",
+                                                        "Mean (SD)" = "<mean> (<sd>)"),
+                                 renameColumns = c("Database name" = "cdm_name"),
+                                 header = c("cdm_name"),
+                                 groupColumn = visOmopResults::strataColumns(summarisedPopulationCharacteristics))
 
 }
