370014b54d0e7e3e05ee59c804b7c89e3f9bd6dd
Marta Alcalde-Herraiz <marta.alcalde@estudiantat.upc.edu>
2024-09-17 13:23:43 +0100

    Update summariseOmopSnapshot and tableOmopSnapshot

--- R/summariseOmopSnapshot.R
+++ R/summariseOmopSnapshot.R
@@ -16,8 +16,9 @@
 summariseOmopSnapshot <- function(cdm) {
 
   omopgenerics::validateCdmArgument(cdm)
-  summaryTable <- summary(cdm)
-  summaryTable <- summaryTable |>
+
+  summaryTable <- summary(cdm) |>
+    internalTibble() |>
     omopgenerics::newSummarisedResult(settings = dplyr::tibble(
       result_id = unique(summaryTable$result_id),
       package_name = "omopSketch",
@@ -25,5 +26,30 @@ summariseOmopSnapshot <- function(cdm) {
       result_type = "summarise_omop_snapshot"
     ))
 
+
   return(summaryTable)
+}
+
+
+internalTibble <- function(summaryTable){
+  summaryTable |>
+    dplyr::inner_join(
+      dplyr::tibble(
+        "variable_name" = c("person_count", "snapshot_date", "vocabulary",
+                            "cdm", "cdm", "cdm", "cdm", "cdm", "cdm", "cdm",
+                            "observation_period_count", "observation_period_start_date", "observation_period_end_date"),
+        "estimate_name" = c("count", "value", "version",
+                            "description", "documentation_reference", "holder_name", "release_date", "source_name","source_type","version",
+                            "count", "min","max"),
+        "variable_name1" = c("general", "general", "general",
+                             "cdm", "cdm", "cdm", "cdm", "cdm", "cdm", "cdm",
+                             "observation_period", "observation_period", "observation_period"),
+        "estimate_name1" = c("person_count", "snapshot_date", "vocabulary_version",
+                             "description", "documentation_reference", "holder_name", "release_date", "source_name","source_type","version",
+                             "count", "start_date", "end_date")
+      ),
+      by = c("variable_name", "estimate_name")
+    ) |>
+    dplyr::select(-c("variable_name", "estimate_name")) |>
+    dplyr::rename("variable_name" = "variable_name1", "estimate_name" = "estimate_name1")
 }
--- R/tableOmopSnapshot.R
+++ R/tableOmopSnapshot.R
@@ -1,6 +1,6 @@
 #' Create a gt table from a summarised omop snapshot
 #'
-#' @param omopSnapshot  Output from summariseOmopSnapshot()
+#' @param result  Output from summariseOmopSnapshot()
 #'
 #' @return A gt object with the summarised data.
 #' @export
@@ -16,53 +16,33 @@
 #' summariseOmopSnapshot() |>
 #' tableOmopSnapshot()
 #'}
-tableOmopSnapshot <- function(summarisedOmopSnapshot){
-  # Initial checks ----
-  omopgenerics::assertClass(summarisedOmopSnapshot, "summarised_result")
+tableOmopSnapshot <- function(result){
 
-  if(summarisedOmopSnapshot |> dplyr::tally() |> dplyr::pull("n") == 0){
-    cli::cli_warn("summarisedOmopSnapshot is empty.")
+  # Initial checks ----
+  omopgenerics::validateResultArguemnt(result)
+
+  if(result |> dplyr::tally() |> dplyr::pull("n") == 0){
+    cli::cli_warn("result is empty.")
 
     return(
-      summarisedOmopSnapshot |>
+      result |>
         visOmopResults::splitGroup() |>
-        dplyr::select("Estimate" = "variable_name", "cdm_name") |>
+        dplyr::select("Variable" = "variable_name", "cdm_name") |>
         gt::gt()
     )
   }
 
-  t <- dplyr::tibble(
-    "variable_name" = c("person_count", "snapshot_date", "vocabulary",
-                        "cdm", "cdm", "cdm", "cdm", "cdm", "cdm", "cdm",
-                        "observation_period_count", "observation_period_start_date", "observation_period_end_date"),
-    "estimate_name" = c("count", "value", "version",
-                        "description", "documentation_reference", "holder_name", "release_date", "source_name","source_type","version",
-                        "count", "min","max"),
-    "var" = c("General", "General", "General",
-              "CDM", "CDM", "CDM", "CDM", "CDM", "CDM", "CDM",
-              "Observation period", "Observation period", "Observation period"),
-    "Estimate" = c("Person count", "Snapshot date", "Vocabulary version",
-           "Description", "Documentation reference", "Holder_name", "Release date", "Source name","Source type","Version",
-           "Count", "Start date", "End date")
-  ) |>
-    dplyr::full_join(
-      summarisedOmopSnapshot |>
-        visOmopResults::splitAll(),
-      by = c("variable_name", "estimate_name")
+  result <- result |>
+    visOmopResults::filterSettings(result_type == "summarise_omop_snapshot") |>
+    dplyr::mutate(variable_name = gsub("_", " ", stringr::str_to_sentence(.data$variable_name)),
+                  estimate_name = gsub("_", " ", stringr::str_to_sentence(.data$estimate_name))) |>
+    visOmopResults::visOmopTable(
+      hide = c("variable_level"),
+      formatEstimateName = c("N" = "<Count>"),
+      header = c("cdm_name"),
+      renameColumns = c("Database name" = "cdm_name", "Estimate" = "estimate_name"),
+      groupColumn = "variable_name"
     )
 
-  options(warn=-1)
-  t <- t |>
-      dplyr::mutate(estimate_value = dplyr::if_else(.data$estimate_name == "count",
-                                                  format(as.numeric(.data$estimate_value), big.mark = ",", digits = 1),
-                                                  .data$estimate_value)
-    )
-  options(warn=0)
-
-  t <- t |>
-    dplyr::select(-c("result_id", "variable_name", "variable_level", "estimate_name", "estimate_type")) |>
-    visOmopResults::formatHeader(header = "cdm_name") |>
-    visOmopResults::gtTable(groupColumn = "var", groupOrder = c("General", "CDM", "Observation period"))
-
-  return(t)
+  return(result)
 }
