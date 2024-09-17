c8ac74626bcdb4dced1d2cdc99abf0d44b49c79a
Marta Alcalde-Herraiz <marta.alcalde@estudiantat.upc.edu>
2024-09-17 18:33:39 +0100

    Solve mixed pr

--- R/summariseInObservation.R
+++ R/summariseInObservation.R
@@ -3,7 +3,7 @@
 #' @param observationPeriod observation_period omop table.
 #' @param unit Whether to stratify by "year" or by "month".
 #' @param unitInterval Number of years or months to include within the time interval.
-#' @param output Output format. It can be either the number of subjects ("subjects") that are in observation in the specific interval of time, the number of person-days ("person-days"), or both c("subjects", "person-days).
+#' @param output Output format. It can be either the number of records ("records") that are in observation in the specific interval of time, the number of person-days ("person-days"), or both ("all").
 #' @param ageGroup A list of age groups to stratify results by.
 #' @param sex Boolean variable. Whether to stratify by sex (TRUE) or not (FALSE).
 #'
@@ -14,7 +14,7 @@
 summariseInObservation <- function(observationPeriod,
                                    unit = "year",
                                    unitInterval = 1,
-                                   output = "subjects",
+                                   output = "records",
                                    ageGroup = NULL,
                                    sex = FALSE){
 
@@ -79,14 +79,14 @@ summariseInObservation <- function(observationPeriod,
 }
 
 getDenominator <- function(cdm, output){
-  if(output == "subjects"){
+  if(output == "records"){
     tibble::tibble(
       "denominator" = c(cdm[["person"]] |>
                           dplyr::ungroup() |>
                           dplyr::select("person_id") |>
                           dplyr::summarise("n" = dplyr::n()) |>
                           dplyr::pull("n")),
-      "variable_name" = "number_subjects")
+      "variable_name" = "records")
   }else if(output == "person-days"){
     y <- cdm[["observation_period"]] |>
       dplyr::ungroup() |>
@@ -115,7 +115,7 @@ getDenominator <- function(cdm, output){
                           dplyr::pull("n"),
                         y
                         ),
-      "variable_name" = c("number_subjects","person-days"))
+      "variable_name" = c("records","person-days"))
   }
 }
 
