7c1ac480f4ebf52811c0b6c11e014b0d61491339
catalamarti <marticatalasabate@gmail.com>
2024-09-18 11:04:42 +0200

    warn not included columns

--- R/utilities.R
+++ R/utilities.R
@@ -16,3 +16,28 @@ typeConcept <- function(name) {
 tableId <- function(name) {
   tables$id[tables$table_name == name]
 }
+
+warnFacetColour <- function(result, cols) {
+  colsToWarn <- result |>
+    dplyr::select(
+      "cdm_name", "group_name", "group_level", "strata_name", "strata_level",
+      "variable_name", "variable_level", "additional_name", "additional_level"
+    ) |>
+    dplyr::distinct() |>
+    visOmopResults::splitAll() |>
+    dplyr::select(!dplyr::any_of(unique(unlist(cols)))) |>
+    as.list() |>
+    purrr::map(unique)
+  colsToWarn <- colsToWarn[lengths(colsToWarn) > 1]
+  if (length(colsToWarn) > 0) {
+    cli::cli_warn(message = c(
+      "{.var {names(colsToWarn)}} not included in {collapseStr(names(cols), 'or')}, but have multiple values."
+    ))
+  }
+  invisible(NULL)
+}
+collapseStr <- function(x, sep) {
+  if (length(x) == 1) return(x)
+  len <- length(x)
+  paste0(paste0(x[-len], collapse = ", "), " ", sep, " ", x[len])
+}
