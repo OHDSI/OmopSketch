fae9ba4a44dc9280e8d8a75ae5b85a4dec223b37
cecicampanile <campanile.cecilia@gmail.com>
2024-10-24 12:37:47 +0100

    Update summariseMissingData.R
    casted na_count to double for the pivot

--- R/summariseMissingData.R
+++ R/summariseMissingData.R
@@ -13,10 +13,10 @@
 #' @export
 
 summariseMissingData <- function(cdm,
-                           omopTableName,
-                           col = NULL,
-                           sex = FALSE,
-                           ageGroup = NULL){
+                                 omopTableName,
+                                 col = NULL,
+                                 sex = FALSE,
+                                 ageGroup = NULL){
 
 
   omopgenerics::validateCdmArgument(cdm)
@@ -32,71 +32,75 @@ summariseMissingData <- function(cdm,
 
   result_tables <-  purrr::map(omopTableName, function(table) {
 
-  omopTable <- cdm[[table]]
-  col_table <- intersect(col, colnames(omopTable))
-  if (is.null(col_table) | rlang::is_empty(col_table)){
-  col_table<-colnames(omopTable)
-  }
+    omopTable <- cdm[[table]]
+    col_table <- intersect(col, colnames(omopTable))
+    if (is.null(col_table) | rlang::is_empty(col_table)){
+      col_table<-colnames(omopTable)
+    }
 
-  indexDate <- startDate(omopgenerics::tableName(omopTable))
-  x <- omopTable |> PatientProfiles::addDemographicsQuery(age = FALSE, ageGroup = ageGroup, sex = sex, indexDate = indexDate)
+    indexDate <- startDate(omopgenerics::tableName(omopTable))
+    x <- omopTable |> PatientProfiles::addDemographicsQuery(age = FALSE, ageGroup = ageGroup, sex = sex, indexDate = indexDate)
 
 
-  result_columns <- purrr::map(col_table, function(c) {
+    result_columns <- purrr::map(col_table, function(c) {
 
-    stratified_result <- x |>
-      dplyr::group_by(dplyr::across(dplyr::all_of(strata))) |>
-      dplyr::summarise(
-        na_count = sum(as.integer(is.na(.data[[c]]))), #try is.numeric
-        total_count = dplyr::n(),
-        .groups = "drop"
-      ) |>
-      dplyr::collect()
-
-    # Group results for each level of stratification
-    grouped_results <- purrr::map(stratification, function(g) {
-      stratified_result |>
-        dplyr::group_by(dplyr::across(dplyr::all_of(g))) |>
+      stratified_result <- x |>
+        dplyr::group_by(dplyr::across(dplyr::all_of(strata))) |>
         dplyr::summarise(
-          na_count = sum(.data$na_count, na.rm = TRUE),
-          total_count = sum(.data$total_count, na.rm = TRUE),
-          colName = c,
+          na_count = sum(as.integer(is.na(.data[[c]]))), #try is.numeric
+          total_count = dplyr::n(),
           .groups = "drop"
         ) |>
-        dplyr::mutate(na_percentage = dplyr::if_else(.data$total_count > 0, (.data$na_count / .data$total_count) * 100, 0))
+        dplyr::collect()
+
+      # Group results for each level of stratification
+      grouped_results <- purrr::map(stratification, function(g) {
+        stratified_result |>
+          dplyr::group_by(dplyr::across(dplyr::all_of(g))) |>
+          dplyr::summarise(
+            na_count = sum(.data$na_count, na.rm = TRUE),
+            total_count = sum(.data$total_count, na.rm = TRUE),
+            colName = c,
+            .groups = "drop"
+          ) |>
+          dplyr::mutate(na_percentage = dplyr::if_else(.data$total_count > 0, (.data$na_count / .data$total_count) * 100, 0))
+      })
+
+      return(purrr::reduce(grouped_results, dplyr::bind_rows))
+
     })
 
-    return(purrr::reduce(grouped_results, dplyr::bind_rows))
-
-  })
-
-  res <- purrr::reduce(result_columns, dplyr::union)|>
-    dplyr::mutate(omop_table = table)
+    res <- purrr::reduce(result_columns, dplyr::union)|>
+      dplyr::mutate(omop_table = table)
 
 
 
-  required_cols <- omopgenerics::omopTableFields(CDMConnector::cdmVersion(cdm))|>
-    dplyr::filter(.data$cdm_table_name==table)|>
-    dplyr::filter(.data$is_required==TRUE)|>
-    dplyr::pull(.data$cdm_field_name)
-  warning_columns <- res |>
-    dplyr::filter(.data$colName %in% required_cols)|>
-    dplyr::filter(.data$na_count>0)|>
-    dplyr::distinct(.data$colName)|>
-    dplyr::pull()
+    required_cols <- omopgenerics::omopTableFields(CDMConnector::cdmVersion(cdm))|>
+      dplyr::filter(.data$cdm_table_name==table)|>
+      dplyr::filter(.data$is_required==TRUE)|>
+      dplyr::pull(.data$cdm_field_name)
+    warning_columns <- res |>
+      dplyr::filter(.data$colName %in% required_cols)|>
+      dplyr::filter(.data$na_count>0)|>
+      dplyr::distinct(.data$colName)|>
+      dplyr::pull()
 
-  if (length(warning_columns) > 0) {
-    cli::cli_warn(c(
-      "These columns contain missing values, which are not permitted:",
-      "{.val {warning_columns}}"
-    ))
-  }
-  return(res)
+    if (length(warning_columns) > 0) {
+      cli::cli_warn(c(
+        "These columns contain missing values, which are not permitted:",
+        "{.val {warning_columns}}"
+      ))
+    }
+    return(res)
   })
 
 
   result<-purrr::reduce(result_tables, dplyr::union)|>
     dplyr::mutate(dplyr::across(dplyr::all_of(strata), ~ dplyr::coalesce(., "overall")))|>
+    dplyr::mutate(
+      na_count = as.double(.data$na_count),     # Cast na_count to double
+      na_percentage = as.double(.data$na_percentage)  # Ensure na_percentage is double as well
+    )|>
     tidyr::pivot_longer(
       cols = c(.data$na_count, .data$na_percentage),
       names_to = "estimate_name",
@@ -116,7 +120,7 @@ summariseMissingData <- function(cdm,
       "estimate_value" = as.character(.data$estimate_value),
       "estimate_type" = "integer",
       "variable_level" = NA
-      ) |>
+    ) |>
     dplyr::rename("variable_name" = "colName") |>
     dplyr::select(!c(.data$total_count))
 
