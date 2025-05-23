ad00dfb32b9033b750a4fb9a8fa8906e844883ea
cecicampanile <campanile.cecilia@gmail.com>
2025-05-23 16:07:52 +0100

    discard previous changes in summariseInObservation

--- R/summariseInObservation.R
+++ R/summariseInObservation.R
@@ -413,14 +413,10 @@ createSummarisedResultAge <- function(observationPeriod, cdm, start_date_name, e
   }
 
   res <- purrr::map(strata, \(stratax) {
-    browser()
+
     x |>
       dplyr::group_by(dplyr::across(dplyr::all_of(c("age_group", stratax, additional_column)))) |>
-      PatientProfiles::summariseResult(
-        estimates = "median", variables = "age", strata = c("age_group", stratax, additional_column)
-      ) |>
-      omopgenerics::tidy()
-      #dplyr::summarise(estimate_value = stats::median(.data$age), .groups = "drop") |>
+      dplyr::summarise(estimate_value = stats::median(.data$age), .groups = "drop") |>
       dplyr::collect()
   }) |>
     dplyr::bind_rows() |>
