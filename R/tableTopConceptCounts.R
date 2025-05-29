b0777e39e0c778be632a663a08b914f6a55cdec2
catalamarti <marticatalasabate@gmail.com>
2025-05-29 01:37:26 +0100

    add countBy argument and internal refactor

--- R/tableTopConceptCounts.R
+++ R/tableTopConceptCounts.R
@@ -3,6 +3,8 @@
 #'
 #' @param result A `summarised_result` object, typically returned by `summariseConceptIdCounts()`.
 #' @param top Integer. The number of top concepts to display. Defaults to `10`.
+#' @param countBy Either 'person' or 'record'. If NULL whatever is in the data
+#' is used.
 #' @param type Character. The output table format. Defaults to `"gt"`. Use `visOmopResults::tableType()` to see all supported formats.
 #'
 #' @return A formatted table object displaying the top concepts from the summarised data.
@@ -15,17 +17,22 @@
 #' library(duckdb)
 #'
 #' requireEunomia()
-#' con <- dbConnect(duckdb(), eunomiaDir())
+#' con <- dbConnect(drv = duckdb(dbdir = eunomiaDir()))
 #' cdm <- cdmFromCon(con = con, cdmSchema = "main", writeSchema = "main")
 #'
-#' result <- summariseConceptIdCounts(cdm, "condition_occurrence")
-#' result |> tableTopConceptCounts(top = 5)
+#' result <- summariseConceptIdCounts(cdm = cdm, omopTableName = "condition_occurrence")
+#'
+#' tableTopConceptCounts(result = result, top = 5)
 #' }
-tableTopConceptCounts <- function(result, top = 10, type = "gt") {
+tableTopConceptCounts <- function(result,
+                                  top = 10,
+                                  countBy = NULL,
+                                  type = "gt") {
 
+  # initial checks
   rlang::check_installed("visOmopResults")
   omopgenerics::validateResultArgument(result)
-  omopgenerics::assertNumeric(top, unique = TRUE, integerish = TRUE, min = 1, length = 1 )
+  omopgenerics::assertNumeric(top, integerish = TRUE, min = 1, length = 1)
   omopgenerics::assertChoice(type, visOmopResults::tableType())
 
   strata_cols <- omopgenerics::strataColumns(result)
@@ -41,68 +48,75 @@ tableTopConceptCounts <- function(result, top = 10, type = "gt") {
     return(emptyTable(type))
   }
 
+  # check countBy
+  result <- result |>
+    dplyr::mutate(estimate_name = dplyr::case_when(
+      .data$estimate_name == "count_records" ~ "record",
+      .data$estimate_name == "count_subjects" ~ "person",
+      .default = .data$estimate_name
+    ))
+  opts <- unique(result$estimate_name)
+  if (length(opts) == 1 & is.null(countBy)) {
+    countBy <- opts
+  }
+  omopgenerics::assertChoice(countBy, opts, length = 1)
 
-  res <- result |>
+  # tidy version
+  result <- result |>
     omopgenerics::splitAll() |>
-    dplyr::mutate(estimate_value = as.numeric(.data$estimate_value)) |>
-    tidyr::pivot_wider(
-      names_from = "estimate_name",
-      values_from = "estimate_value"
+    omopgenerics::pivotEstimates() |>
+    dplyr::select(!"result_id") |>
+    dplyr::rename(
+      count = dplyr::all_of(countBy),
+      standard_concept_name = "variable_name",
+      standard_concept_id = "variable_level"
     )
-    estimateForm <- "Standard: %s (%s); Source: %s (%s); %i"
-    if (type == "gt") {
-      estimateForm <- stringr::str_replace_all(estimateForm, ";", " <br>")
-    }
 
-  if ("count_records" %in% colnames(res)) {
-    records <- res |>
-      dplyr::group_by(dplyr::across(dplyr::any_of(c("time_interval", "sex", "age_group")))) |>
-      dplyr::arrange(dplyr::desc(.data$count_records)) |>
-      dplyr::slice_head(n = top) |>
-      dplyr::mutate("top" = dplyr::row_number()) |>
-      dplyr::ungroup() |>
-      dplyr::mutate(estimate_value = sprintf(
-        .env$estimateForm, .data$variable_name, .data$variable_level,
-        .data$source_concept_name, .data$source_concept_id, .data$count_records
-      ),
-      estimate_name = "N records")
-
-  } else {
-    records <- tibble::tibble()
+  # estimate name
+  estimateForm <- "Standard: %s (%s); Source: %s (%s); %i"
+  if (type == "gt") {
+    estimateForm <- stringr::str_replace_all(estimateForm, ";", " <br>")
   }
-  if ("count_subjects" %in% colnames(res)) {
-    subjects <- res |>
-      dplyr::group_by(dplyr::across(dplyr::any_of(c("time_interval", "sex", "age_group", "estimate_name")))) |>
-      dplyr::arrange(dplyr::desc(.data$count_subjects)) |>
-      dplyr::slice_head(n = top) |>
-      dplyr::mutate("top" = dplyr::row_number()) |>
-      dplyr::ungroup() |>
-      dplyr::mutate(estimate_value = sprintf(
-        .env$estimateForm, .data$variable_name, .data$variable_level,
-        .data$source_concept_name, .data$source_concept_id, .data$count_subjects
-      ),
-      estimate_name = "N subjects" )
-  } else {
-    subjects <- tibble::tibble()
-  }
-  res <- dplyr::bind_rows(records, subjects) |>
-    dplyr::mutate(estimate_type = "character") |>
-    dplyr::select(dplyr::any_of(c("estimate_name","cdm_name", "omop_table", additional_cols, strata_cols, "estimate_value", "estimate_type", "top")))
 
+  # format data
+  colsGroup <- c(
+    "standard_concept_name", "standard_concept_id", "source_concept_name",
+    "source_concept_id", "count"
+  )
+  result <- result |>
+    dplyr::group_by(dplyr::across(!dplyr::all_of(colsGroup))) |>
+    dplyr::arrange(dplyr::desc(.data$count)) |>
+    dplyr::slice_head(n = top) |>
+    dplyr::mutate("top" = dplyr::row_number()) |>
+    dplyr::ungroup() |>
+    dplyr::mutate(
+      estimate_value = sprintf(
+        .env$estimateForm, .data$standard_concept_name,
+        .data$standard_concept_id, .data$source_concept_name,
+        .data$source_concept_id, .data$count
+      ),
+      estimate_name = "counts",
+      estimate_type = "character"
+    ) |>
+    dplyr::select(!dplyr::all_of(colsGroup))
+
+  # create visual table with visOmopResults
   header <- c("cdm_name", additional_cols)
   group <- c("omop_table", strata_cols)
+  tab <- result |>
+    visOmopResults::visTable(
+      header = header,
+      estimateName = NULL,
+      hide = c("estimate_name", "estimate_type"),
+      group = group,
+      .options = list(merge = "all_columns"),
+      type = type
+    )
 
-  tab <- visOmopResults::visTable(res,
-                           header = header,
-                           hide = "estimate_type",
-                           group = group,
-                           .options = list(merge = "all_columns"),
-                           type = type)
+  # add line breaks if gt table
   if (type == "gt") {
     tab <- gt::fmt_markdown(tab)
   }
 
-  return(tab |> suppressMessages())
-
-
+  return(tab)
 }
