7256765bc4b4741743882f52b0e80e2ff4437040
catalamarti <marticatalasabate@gmail.com>
2025-07-01 19:47:07 +0100

    add tryCatch and not characterise those that can not be downloaded

--- extras/createShiny.R
+++ extras/createShiny.R
@@ -13,39 +13,44 @@ results <- purrr::map(databases, \(dbName) {
   cli::cli_inform(c(i = "Starting {.strong {dbName}} characterisation"))
 
   # download dataset
-  omock::downloadMockDataset(datasetName = dbName, overwrite = FALSE) |>
-    suppressMessages()
-  cli::cli_inform(c(v = "{.strong {dbName}} downloaded."))
+  res <- tryCatch(expr = {
+    omock::downloadMockDataset(datasetName = dbName, overwrite = FALSE) |>
+      suppressMessages()
+    cli::cli_inform(c(v = "{.strong {dbName}} downloaded."))
 
-  # create cdm reference
-  duckFile <- tempfile(fileext = ".duckdb")
-  drv <- duckdb::duckdb(dbdir = duckFile)
-  src <- CDMConnector::dbSource(con = duckdb::dbConnect(drv = drv), writeSchema = "main")
-  cdm <- omock::mockCdmFromDataset(datasetName = dbName) |>
-    omopgenerics::insertCdmTo(to = src) |>
-    suppressMessages()
-  cli::cli_inform(c(v = "{.cls cdm_reference} created for {.strong {dbName}}."))
+    # create cdm reference
+    duckFile <- tempfile(fileext = ".duckdb")
+    drv <- duckdb::duckdb(dbdir = duckFile)
+    src <- CDMConnector::dbSource(con = duckdb::dbConnect(drv = drv), writeSchema = "main")
+    cdm <- omock::mockCdmFromDataset(datasetName = dbName) |>
+      omopgenerics::insertCdmTo(to = src) |>
+      suppressMessages()
+    cli::cli_inform(c(v = "{.cls cdm_reference} created for {.strong {dbName}}."))
 
-  # characterise databas
-  start <- Sys.time()
-  res <- databaseCharacteristics(
-    cdm = cdm,
-    interval = "years",
-    conceptIdCounts = TRUE
-  ) |>
-    suppressMessages()
-  diff <- difftime(time1 = Sys.time(), time2 = start, units = "secs") |>
-    as.numeric() |>
-    round()
-  cli::cli_inform(c(v = "{.strong {dbName}} characterised in {diff} seconds."))
+    # characterise databas
+    start <- Sys.time()
+    x <- databaseCharacteristics(
+      cdm = cdm,
+      interval = "years",
+      conceptIdCounts = TRUE
+    ) |>
+      suppressMessages()
+    diff <- difftime(time1 = Sys.time(), time2 = start, units = "secs") |>
+      as.numeric() |>
+      round()
+    cli::cli_inform(c(v = "{.strong {dbName}} characterised in {diff} seconds."))
 
-  # disconnect
-  CDMConnector::cdmDisconnect(cdm = cdm)
-  duckdb::duckdb_shutdown(drv = drv)
-  unlink(duckFile)
-  unlink(file.path(omock::mockDatasetsFolder(), paste0(dbName, ".zip")))
+    # disconnect
+    CDMConnector::cdmDisconnect(cdm = cdm)
+    duckdb::duckdb_shutdown(drv = drv)
+    unlink(duckFile)
+    unlink(file.path(omock::mockDatasetsFolder(), paste0(dbName, ".zip")))
 
-  res
+    x
+  }, error = function(e) {
+    cli::cli_inform(c("x" = "{dbName} could not be characterised"))
+    omopgenerics::emptySummarisedResult()
+  })
 }) |>
   omopgenerics::bind()
 
